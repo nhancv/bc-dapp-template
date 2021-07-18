8461f64a05d879b1a0173728c9dc68ee58be7c0f
Nhan Cao <nhancv92@gmail.com>
2021-07-18 15:37:04 +0700

    feat: update

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const CryptoLott = artifacts.require("CryptoLott");
 const UniqueAsset = artifacts.require("UniqueAsset");
 const nhancv = artifacts.require("nhancv");
 
-module.exports = function(deployer) {
+module.exports = async (deployer, network, accounts) => {
   deployer.deploy(CryptoLott);
   deployer.deploy(UniqueAsset);
   deployer.deploy(nhancv);
