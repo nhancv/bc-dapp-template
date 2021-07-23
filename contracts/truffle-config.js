5ac301b3371c2c7df39ee772cfd76beb35b571a7
Nhan Cao <nhancv92@gmail.com>
2021-07-23 14:09:07 +0700

    feat: update

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -123,6 +123,8 @@ module.exports = {
     bscTestnet:{
       provider: () => new HDWalletProvider(MNEMONIC, "https://data-seed-prebsc-1-s1.binance.org:8545"),
       network_id: 97,
+      gas: 4698712,
+      gasPrice: 25000000000
     },
     // bsc main net
     bscMainnet: {
@@ -151,13 +153,13 @@ module.exports = {
     solc: {
       version: "0.8.3",
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
     }
   },
 
